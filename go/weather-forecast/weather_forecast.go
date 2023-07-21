b38df377a670a94b03f3e1e5779192050a05caf8
yeahokyok <982466+yeahokyok@users.noreply.github.com>
2023-07-21 11:57:22 +0700

    weather

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather should have a comment.
 package weather
 
+// CurrentCondition is the current weather condition.
 var CurrentCondition string
+
+// CurrentLocation is the current weather location.
 var CurrentLocation string
 
+// Forecast returns a weather forecast for a given location and condition.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
