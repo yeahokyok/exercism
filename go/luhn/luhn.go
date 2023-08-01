fa283bad5a5fdc1890c6dd5ed8afc65564dcb8d8
yeahokyok <982466+yeahokyok@users.noreply.github.com>
2023-08-01 11:52:31 +0700

    luhn refactored

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,77 +1,34 @@
 package luhn
 
 import (
-	"regexp"
-	"strconv"
 	"strings"
+	"unicode"
 )
 
-func is_numeric(word string) bool {
-	return regexp.MustCompile(`\d`).MatchString(word)
-}
-
-func Valid(id string) bool {
-
-	id = strings.ReplaceAll(id, " ", "")
-	if !is_numeric(id) {
+func Valid(input string) bool {
+	input = strings.ReplaceAll(input, " ", "")
+	if len(input) <= 1 {
 		return false
-	}
-
-	if len(id) <= 1 {
-		return false
-	}
-
-	doubled := make([]int, len(id))
-	isOddID := false
-	if len(id)%2 != 0 {
-		isOddID = true
-	}
-
-	for i := len(doubled) - 1; i >= 0; i-- {
-		if isOddID {
-			if i%2 == 0 {
-				val, err := strconv.Atoi(string(id[i]))
-				if err != nil {
-					return false
-				}
-				doubled[i] = val
-				continue
-			} else {
-				val, err := strconv.Atoi(string(id[i]))
-				if err != nil {
-					return false
-				}
-				val *= 2
-				if val > 9 {
-					val -= 9
-				}
-				doubled[i] = val
-				continue
-			}
-
-		}
-		if i%2 == 0 {
-			val, err := strconv.Atoi(string(id[i]))
-			if err != nil {
-				return false
-			}
-			val *= 2
-			if val > 9 {
-				val -= 9
-			}
-			doubled[i] = val
-			continue
-		}
-		val, err := strconv.Atoi(string(id[i]))
-		if err != nil {
-			return false
-		}
-		doubled[i] = val
 	}
 
 	sum := 0
-	for _, num := range doubled {
-		sum += int(num)
+	shouldDouble := false
+	for i := len(input) - 1; i >= 0; i-- {
+		if !unicode.IsDigit(rune(input[i])) { // check if not a digit
+			return false
+		}
+
+		num := int(input[i] - '0') // convert to integer
+
+		if shouldDouble {
+			num *= 2
+			if num > 9 {
+				num -= 9
+			}
+		}
+
+		sum += num
+		shouldDouble = !shouldDouble // toggle the double flag
 	}
 
 	return sum%10 == 0
