c9643af5dd5fed2475ae7cbdaf0f57ece916a7db
yeahokyok <982466+yeahokyok@users.noreply.github.com>
2023-08-01 12:09:32 +0700

    Space Age

---
+++ go/space-age/space_age.go
@@ -0,0 +1,27 @@
+package space
+
+type Planet string
+
+func Age(seconds float64, planet Planet) float64 {
+	earthYears := seconds / 31557600
+	switch planet {
+	case "Mercury":
+		return earthYears / 0.2408467
+	case "Venus":
+		return earthYears / 0.61519726
+	case "Earth":
+		return earthYears
+	case "Mars":
+		return earthYears / 1.8808158
+	case "Jupiter":
+		return earthYears / 11.862615
+	case "Saturn":
+		return earthYears / 29.447498
+	case "Uranus":
+		return earthYears / 84.016846
+	case "Neptune":
+		return earthYears / 164.79132
+	default:
+		return -1
+	}
+}
